91ecee166d39288d6067bc23fededdef06c9ce5b
BettyPan <betty19931218@gmail.com>
2020-12-25 13:54:16 +0800

    Initial Commit

---
+++ mathGameForKids/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mathGameForKids
+//
+//  Created by BettyPan on 2020/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
